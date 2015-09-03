87c11702edf3d9bb7dc5fca530822f0009b721bb
Daniel Kutik <daniel@kutik.eu>
2015-09-03 18:38:54 +0200

    ugly background thread that restarts the listener

--- app/src/main/java/eu/kutik/accelerometertest/TestService.java
+++ app/src/main/java/eu/kutik/accelerometertest/TestService.java
@@ -1,6 +1,8 @@
 package eu.kutik.accelerometertest;
 
+import android.app.Notification;
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -10,25 +12,82 @@ import android.hardware.SensorManager;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.PowerManager;
+import android.os.Process;
 import android.support.annotation.Nullable;
 import android.util.Log;
+
 
 public class TestService extends Service {
 
   private static final String TAG = TestService.class.getSimpleName();
 
-  private static final int SENSOR_DELAY = SensorManager.SENSOR_DELAY_FASTEST;
+  private static final int SENSOR_DELAY = SensorManager.SENSOR_DELAY_NORMAL;
+  private static final int SCREEN_OFF_DELAY = 50; // maybe a little higher value here
 
   private TestListener listener = null;
 
-  private ScreenOffReciever screenOffReceiver;
+  private BroadcastReceiver screenEventReceiver = new BroadcastReceiver() {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+      if (intent != null) {
+        if (Intent.ACTION_SCREEN_OFF.equals(intent.getAction())) {
+          onScreenOff();
+        } else if (Intent.ACTION_SCREEN_ON.equals(intent.getAction())) {
+          onScreenOn();
+        }
+      }
+    }
+  };
+
+  Thread screenOffThread = null;
+  final Runnable screenOffRunnable = new Runnable() {
+    @Override
+    public void run() {
+      //noinspection InfiniteLoopStatement
+      while (screenOffThread != null) {
+        //NO LOGGING inside this loop!
+        registerDropListenerScreenOff();
+        try {
+          Thread.sleep(SCREEN_OFF_DELAY);
+        } catch (InterruptedException ignore) { }
+        unregisterDropListener();
+      }
+    }
+  };
+
+  private void startScreenOffThread() {
+    screenOffThread = new Thread(screenOffRunnable);
+    screenOffThread.start();
+  }
+
+  private void stopScreenOffThread() {
+    if (screenOffThread != null) {
+      if (!screenOffThread.isInterrupted()) {
+        screenOffThread.interrupt();
+      }
+      screenOffThread = null;
+    }
+  }
+
+
+  private void onScreenOff() {
+    unregisterDropListener();
+    stopScreenOffThread();
+    startScreenOffThread();
+  }
+
+  private void onScreenOn() {
+    stopScreenOffThread();
+    unregisterDropListener();
+    registerDropListener();
+  }
 
   private PowerManager.WakeLock sensorWakeLock;
 
   @Nullable
   private SensorManager sensorManager = null;
 
-  public static void start(@Nullable final Context context, @Nullable final Intent intent) {
+  public static void start(@Nullable final Context context) {
     Log.wtf(TAG, "#start");
     if (context != null) {
       try {
@@ -46,18 +105,13 @@ public class TestService extends Service {
     return null;
   }
 
+
+
   @Override
   public int onStartCommand(final Intent intent, final int flags, final int startId) {
     Log.wtf(TAG, ".onStartCommand");
 
-    Runnable runnable = new Runnable() {
-      public void run() {
-        Log.wtf(TAG, "Delayed");
-        reregisterDropListener();
-      }
-    };
-
-    new Handler().postDelayed(runnable, 5000);
+    startForeground(Process.myPid(), new Notification());
 
     // We want this service to continue running until it is explicitly
     // stopped, so return sticky.
@@ -70,16 +124,17 @@ public class TestService extends Service {
     initSensorManager();
     initDropListener();
     registerDropListener();
-    registerScreenOffReciever();
-    aquireWakeLock();
+    // TODO check if the service starts while screen is off
+    registerScreenEventReciever();
+    //aquireWakeLock();
   }
 
   @Override
   public void onDestroy() {
     super.onDestroy();
-    unregisterScreenOffReciever();
+    unregisterScreenEventReciever();
     unregisterDropListener();
-    releaseWakeLock();
+    //releaseWakeLock();
   }
 
   private void initDropListener() {
@@ -105,6 +160,14 @@ public class TestService extends Service {
     }
   }
 
+  private void registerDropListenerScreenOff() {
+    if (sensorManager != null && listener != null) {
+      sensorManager.registerListener(listener,
+          sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+          SensorManager.SENSOR_DELAY_FASTEST);
+    }
+  }
+
   private void unregisterDropListener() {
     if (sensorManager != null && listener != null) {
       sensorManager.unregisterListener(listener);
@@ -115,28 +178,22 @@ public class TestService extends Service {
     sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
   }
 
-  private void reregisterDropListener() {
-    //initSensorManager();
-    unregisterDropListener();
-    registerDropListener();
-  }
-
-  private void registerScreenOffReciever() {
-    screenOffReceiver = new ScreenOffReciever();
+  private void registerScreenEventReciever() {
     final IntentFilter intentFilter = new IntentFilter();
     intentFilter.addAction(Intent.ACTION_SCREEN_OFF);
-    registerReceiver(screenOffReceiver, intentFilter);
+    intentFilter.addAction(Intent.ACTION_SCREEN_ON);
+    registerReceiver(screenEventReceiver, intentFilter);
   }
 
-  private void unregisterScreenOffReciever() {
-    unregisterReceiver(screenOffReceiver);
+  private void unregisterScreenEventReciever() {
+    unregisterReceiver(screenEventReceiver);
   }
 
   private void aquireWakeLock() {
     if (sensorWakeLock == null) {
       final PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
       if (pm != null) {
-        sensorWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "gsensor");
+        sensorWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
       }
     }
     if (sensorWakeLock != null) {
